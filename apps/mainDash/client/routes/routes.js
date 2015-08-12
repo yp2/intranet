d1bdaccc49e374943033bde1e476a00f5c9aed60
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2015-08-13 00:31:31 +0200

    route name

--- apps/mainDash/client/routes/routes.js
+++ apps/mainDash/client/routes/routes.js
@@ -1,8 +1,8 @@
 FlowRouter.route("/", {
     action: function (params, queryParams) {
-        console.log("router - Main dash");
         var regions = _.extend({content: "mainDashContent"}, MyApp.mainDashRegions);
-        console.log(MyApp.mainDashRegions);
         BlazeLayout.render('mainDashLayout', regions)
-    }
+    },
+    name: 'mainDash'
+
 });
