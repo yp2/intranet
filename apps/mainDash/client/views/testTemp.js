b8d56a12be4fa293971a4baf943d4f8ed3d8f397
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2015-08-12 22:38:51 +0200

    flow-router plus extras, img src for admin-lte, testTemp - test template for testing

---
+++ apps/mainDash/client/views/testTemp.js
@@ -0,0 +1,21 @@
+Template.testTemp.helpers({
+    //add you helpers here
+});
+
+Template.testTemp.events({
+    //add your events here
+});
+
+Template.testTemp.onCreated(function () {
+    //add your statement here
+    console.log($.AdminLTE);
+});
+
+Template.testTemp.onRendered(function () {
+    
+});
+
+Template.testTemp.onDestroyed(function () {
+    //add your statement here
+});
+
