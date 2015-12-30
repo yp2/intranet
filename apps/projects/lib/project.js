05b8a81916edf33bc7e223ad378903d9ee232846
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2015-12-30 23:17:43 +0100

    es6 fix

--- apps/projects/lib/project.js
+++ apps/projects/lib/project.js
@@ -5,12 +5,16 @@
 Project = new Meteor.Collection('project');
 
 Project.before.insert(function (userId, doc) {
-    var createDate = new Date();
+    "use strict";
+
+    let createDate = new Date();
     doc.createdAt = createDate;
 });
 
 Project.before.update(function (userId, doc, fieldNames, modifier, options) {
-    var modDate = new Date();
+    "use strict";
+
+    let modDate = new Date();
     modifier.$set = modifier.$set || {};
     modifier.$set.modifiedAt = modDate;
     modifier.$set.modifiedBy = userId
