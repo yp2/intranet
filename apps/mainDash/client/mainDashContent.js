0e3df2ff7e6ed9d191c3a32f52c1873f731021d6
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2016-01-31 14:49:06 +0100

    mainDash

--- apps/mainDash/client/mainDashContent.js
+++ apps/mainDash/client/mainDashContent.js
@@ -2,6 +2,24 @@
 
 
 Template.mainDashContent.helpers({
+    projectsCount () {
+        let user = Meteor.user();
+        console.log(Meteor.users.find().fetch());
+        return Project.find({'scope.id': user.profile.scopeSelected.id}).count()
+    },
+    registeredUser () {
+        return Meteor.users.find().count();
+    },
+    wikiCategoriesCount() {
+        return Wiki.findOne({type: 'org'}).categories.length;
+    },
+    wikiProjectCategoriesCount() {
+        let count = 0;
+        Wiki.find({type: 'pro'}).forEach(function (ele) {
+            count = count + ele.categories.length;
+        });
+        return count
+    }
 
 });
 
